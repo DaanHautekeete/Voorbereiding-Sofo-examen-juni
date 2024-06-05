19f6b37f0d4a96a2ba1466f2494af14c1a25e6f5
Daan Hautekeete <hautekeete.daan@gmail.com>
2024-06-05 10:07:29 +0200

    Maken project + toevoegen map
    - Project gemaakt
    - Map voor hoofdstuk 4 - iteraties gemaakt
    - Map voor oefening 2 gemaakt
    
    Voor elke oefeningen wordt er een map gemaakt, zo is het duidelijk waar alle bestanden te vinden zijn

---
+++ Voobereiding SOFO examen juni/Hoofdstuk 4/Iteraties/Oef-2/frmOefening2.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Voobereiding_SOFO_examen_juni.Hoofdstuk_4.Iteraties.Oef_2
+{
+    partial class frmOefening2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSom = new System.Windows.Forms.Label();
+            this.lblIngegevenGetal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSom
+            // 
+            this.lblSom.AutoSize = true;
+            this.lblSom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSom.Location = new System.Drawing.Point(30, 76);
+            this.lblSom.Name = "lblSom";
+            this.lblSom.Size = new System.Drawing.Size(47, 16);
+            this.lblSom.TabIndex = 3;
+            this.lblSom.Text = "uitvoer";
+            // 
+            // lblIngegevenGetal
+            // 
+            this.lblIngegevenGetal.AutoSize = true;
+            this.lblIngegevenGetal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngegevenGetal.Location = new System.Drawing.Point(30, 26);
+            this.lblIngegevenGetal.Name = "lblIngegevenGetal";
+            this.lblIngegevenGetal.Size = new System.Drawing.Size(47, 16);
+            this.lblIngegevenGetal.TabIndex = 2;
+            this.lblIngegevenGetal.Text = "uitvoer";
+            // 
+            // frmOefening2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 199);
+            this.Controls.Add(this.lblSom);
+            this.Controls.Add(this.lblIngegevenGetal);
+            this.Name = "frmOefening2";
+            this.Text = "Hoofdstuk 4 - iteraties - oefening 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSom;
+        private System.Windows.Forms.Label lblIngegevenGetal;
+    }
+}
---
+++ Voobereiding SOFO examen juni/Hoofdstuk 4/Iteraties/Oef-2/frmOefening2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Voobereiding_SOFO_examen_juni.Hoofdstuk_4.Iteraties.Oef_2
+{
+    public partial class frmOefening2 : Form
+    {
+        public frmOefening2()
+        {
+            InitializeComponent();
+        }
+    }
+}
